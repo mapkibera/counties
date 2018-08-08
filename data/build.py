32a3dab965f31727c5e4a1167066f0181e85bbf0
Mikel Maron <mikel_maron@yahoo.com>
2018-08-07 21:58:45 -0400

    added Baringo County

--- data/build.py
+++ data/build.py
@@ -29,16 +29,22 @@ def url2file(url,file_name):
 
 def sync_osm():
   makueni = "37.37,-1.87695,37.73598,-1.59007"
+  baringo = "35.521,-0.215,36.495,1.680"
+
   url_base = "http://overpass-api.de/api/interpreter?data=[bbox];node['wb_pb:id'];out%20meta;&bbox="
   url2file(url_base + makueni,"makueni-projects-osm.xml")
+  url2file(url_base + baringo,"baringo-projects-osm.xml")
 
   url_base = "http://overpass-api.de/api/interpreter?data=[bbox];node[~'.'~'.'];out%20meta;&bbox="
   url2file(url_base + makueni,"makueni-poi-osm.xml")
+  url2file(url_base + baringo,"baringo-poi-osm.xml")
+
 
 def sync_projects():
   url2file('https://docs.google.com/spreadsheets/u/1/d/1xFKs2JLuIqlsvUnORMwbSx_TOSiedI3ISL2HUPk475k/export?format=csv&id=1xFKs2JLuIqlsvUnORMwbSx_TOSiedI3ISL2HUPk475k&gid=1724788530', 'wote-projects-23.csv')
   url2file('https://docs.google.com/spreadsheets/u/1/d/1xFKs2JLuIqlsvUnORMwbSx_TOSiedI3ISL2HUPk475k/export?format=csv&id=1xFKs2JLuIqlsvUnORMwbSx_TOSiedI3ISL2HUPk475k&gid=239806010', 'wote-projects-25.csv')
   url2file('https://docs.google.com/spreadsheets/u/1/d/1xFKs2JLuIqlsvUnORMwbSx_TOSiedI3ISL2HUPk475k/export?format=csv&id=1xFKs2JLuIqlsvUnORMwbSx_TOSiedI3ISL2HUPk475k&gid=2140044208', 'mbooni.csv')
+  url2file('https://docs.google.com/spreadsheets/d/1j5lMG1G0yzk70ujdKK0O9IpjUbgkfB74I5bEjCw6E2s/export?format=csv&id=1j5lMG1G0yzk70ujdKK0O9IpjUbgkfB74I5bEjCw6E2s&gid=502215687', 'baringo-projects.csv')
 
   os.system("tail -n +2 wote-projects-25.csv > wote-projects-25-clipped.csv")
   os.system("tail -n +2 mbooni.csv > mbooni-clipped.csv")
@@ -47,17 +53,18 @@ def sync_projects():
 def convert_geojson():
   os.system("osmtogeojson -e makueni-projects-osm.xml > makueni-projects-osm.geojson")
   os.system("osmtogeojson -e makueni-poi-osm.xml > makueni-poi-osm.geojson")
+  os.system("osmtogeojson -e baringo-projects-osm.xml > baringo-projects-osm.geojson")
+  os.system("osmtogeojson -e baringo-poi-osm.xml > baringo-poi-osm.geojson")
 
-
-def match_projects():
+def match_projects(county):
   result = {}
   result['type'] = 'FeatureCollection'
   result['features'] = []
 
-  f = open('makueni-projects.csv')
+  f = open(county + '-projects.csv')
   reader = csv.DictReader(f)
 
-  osm = geojson.loads(readfile('makueni-projects-osm.geojson'))
+  osm = geojson.loads(readfile(county + '-projects-osm.geojson'))
 
   for row in reader:
     found_match = False
@@ -79,24 +86,26 @@ def match_projects():
       print "WARN: no project id match " +  row['_id']
 
   dump = geojson.dumps(result, sort_keys=True, indent=2)
-  writefile('makueni-projects-matched.geojson',dump)
+  writefile(county + '-projects-matched.geojson',dump)
 
-def filter_poi():
+def filter_poi(county):
    result = {}
    result['type'] = 'FeatureCollection'
    result['features'] = []
 
-   osm = geojson.loads(readfile('makueni-poi-osm.geojson'))
+   osm = geojson.loads(readfile(county + '-poi-osm.geojson'))
    for feature in osm.features:
        if feature.properties['tags'].get('wb_pb:id', None) == None:
            feature.properties['tags']['osm:id'] = feature.properties['id']
            result['features'].append({ "type": "Feature",  "id": feature.properties["id"], "properties": feature.properties['tags'], "geometry": feature.geometry })
 
    dump = geojson.dumps(result, sort_keys=True, indent=2)
-   writefile('makueni-poi-only.geojson',dump)
+   writefile(county + '-poi-only.geojson',dump)
 
 sync_osm()
 sync_projects()
 convert_geojson()
-match_projects()
-filter_poi()
+match_projects('makueni')
+match_projects('baringo')
+filter_poi('makueni')
+filter_poi('baringo')
