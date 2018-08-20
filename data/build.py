0321129ffde22e300fadab1429d74a701c85660d
Mikel Maron <mikel_maron@yahoo.com>
2018-08-20 12:08:53 -0400

    split baringo into kabernet and eldama. assign iterator

--- data/build.py
+++ data/build.py
@@ -31,9 +31,14 @@ def sync_osm():
   makueni = "37.37,-1.87695,37.73598,-1.59007"
   baringo = "35.521,-0.215,36.495,1.680"
 
+  eldama = "35.6881,-0.0307,35.7553,0.0852"
+  kabernet = "35.6147,0.4353,35.7990,0.5916"
+
   url_base = "http://overpass-api.de/api/interpreter?data=[bbox];node['wb_pb:id'];out%20meta;&bbox="
   url2file(url_base + makueni,"makueni-projects-osm.xml")
-  url2file(url_base + baringo,"baringo-projects-osm.xml")
+#  url2file(url_base + baringo,"baringo-projects-osm.xml")
+  url2file(url_base + eldama,"eldama-projects-osm.xml")
+  url2file(url_base + kabernet,"kabernet-projects-osm.xml")
 
   url_base = "http://overpass-api.de/api/interpreter?data=[bbox];node[~'.'~'.'](newer:'2018-05-01T00:00:00Z');out%20meta;&bbox="
   url2file(url_base + makueni,"makueni-poi-osm.xml")
@@ -50,11 +55,19 @@ def sync_projects():
   os.system("tail -n +2 mbooni.csv > mbooni-clipped.csv")
   os.system("cat wote-projects-23.csv wote-projects-25-clipped.csv mbooni-clipped.csv > makueni-projects.csv")
 
+  os.system("cp baringo-projects.csv kabernet-projects.csv")
+  os.system("cp baringo-projects.csv eldama-projects.csv")
+
 def convert_geojson():
   os.system("osmtogeojson -e makueni-projects-osm.xml > makueni-projects-osm.geojson")
   os.system("osmtogeojson -e makueni-poi-osm.xml > makueni-poi-osm.geojson")
-  os.system("osmtogeojson -e baringo-projects-osm.xml > baringo-projects-osm.geojson")
+#  os.system("osmtogeojson -e baringo-projects-osm.xml > baringo-projects-osm.geojson")
+  os.system("osmtogeojson -e kabernet-projects-osm.xml > kabernet-projects-osm.geojson")
+  os.system("osmtogeojson -e eldama-projects-osm.xml > eldama-projects-osm.geojson")
   os.system("osmtogeojson -e baringo-poi-osm.xml > baringo-poi-osm.geojson")
+
+def merge_geojson():
+  os.system("geojson-merge kabernet-projects-matched.geojson eldama-projects-matched.geojson > baringo-projects-matched-merged.geojson")
 
 def match_projects(county):
   result = {}
@@ -102,10 +115,30 @@ def filter_poi(county):
    dump = geojson.dumps(result, sort_keys=True, indent=2)
    writefile(county + '-poi-only.geojson',dump)
 
+def number_projects(county):
+   result = {}
+   result['type'] = 'FeatureCollection'
+   result['features'] = []
+
+   i = 1
+   osm = geojson.loads(readfile(county + '-projects-matched.geojson'))
+   for feature in sorted(osm.features, key=lambda project: project.properties['Project_Name_or_Title'].strip().lower()):
+     feature.properties["iterator"] = i
+     result['features'].append(feature)
+     i += 1
+
+   dump = geojson.dumps(result, sort_keys=True, indent=2)
+   writefile(county + '-projects-numbered.geojson',dump)
+
 sync_osm()
 sync_projects()
 convert_geojson()
 match_projects('makueni')
-match_projects('baringo')
+#match_projects('baringo')
+match_projects('kabernet')
+match_projects('eldama')
+merge_geojson()
 filter_poi('makueni')
 filter_poi('baringo')
+number_projects('kabernet')
+number_projects('eldama')
