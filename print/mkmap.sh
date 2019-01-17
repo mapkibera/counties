ec35fd38a67ce545334bdd38282f333ecf3eaa4e
Mikel Maron <mikel_maron@yahoo.com>
2019-01-16 21:17:54 -0500

    makueni

--- print/mkmap.sh
+++ print/mkmap.sh
@@ -5,4 +5,12 @@
 
 # eldama
 
-perl stitch.pl --lat=2848 --lon=3977089 --zoom=16 --width=3500 --height=3500 --tileBase=https://api.mapbox.com/styles/v1/mikelmaron/cjl5w24tt08852rlqybihghm2/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWlrZWxtYXJvbiIsImEiOiJjaWZlY25lZGQ2cTJjc2trbmdiZDdjYjllIn0.Wx1n0X7aeCQyDTnK6_mrGw --output=eldama.png
+#perl stitch.pl --lat=2848 --lon=3977089 --zoom=16 --width=3500 --height=3500 --tileBase=https://api.mapbox.com/styles/v1/mikelmaron/cjl5w24tt08852rlqybihghm2/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWlrZWxtYXJvbiIsImEiOiJjaWZlY25lZGQ2cTJjc2trbmdiZDdjYjllIn0.Wx1n0X7aeCQyDTnK6_mrGw --output=eldama.png
+
+# mbooni
+
+#perl stitch.pl --lat=-183825 --lon=4166811 --zoom=15 --width=3500 --height=3500 --tileBase=https://api.mapbox.com/styles/v1/mikelmaron/cjla57h440pr92snvo3wh3274/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWlrZWxtYXJvbiIsImEiOiJjaWZlY25lZGQ2cTJjc2trbmdiZDdjYjllIn0.Wx1n0X7aeCQyDTnK6_mrGw --output=mbooni.png
+
+# wote
+
+perl stitch.pl --lat=-201544 --lon=4185225 --zoom=15 --width=3500 --height=3500 --tileBase=https://api.mapbox.com/styles/v1/mikelmaron/cjla8fi2048wg2sk0c9dfy5y5/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWlrZWxtYXJvbiIsImEiOiJjaWZlY25lZGQ2cTJjc2trbmdiZDdjYjllIn0.Wx1n0X7aeCQyDTnK6_mrGw --output=wote.png
