29f1f3380b53d714af6c4db1565a8902fc734e8b
Mikel Maron <mikel_maron@yahoo.com>
2019-03-30 23:47:29 -0400

    print update

--- print/mkmap.sh
+++ print/mkmap.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # kabernet
-#perl stitch.pl --lat=58214 --lon=3973764 --zoom=15 --width=3500 --height=3500 --tileBase=https://api.mapbox.com/styles/v1/mikelmaron/cjl34nd460s3z2tn3hz6hlb9s/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWlrZWxtYXJvbiIsImEiOiJjaWZlY25lZGQ2cTJjc2trbmdiZDdjYjllIn0.Wx1n0X7aeCQyDTnK6_mrGw --output=kabernet.png
+#perl stitch.pl --lat=56214 --lon=3973764 --zoom=15 --width=3500 --height=3500 --tileBase=https://api.mapbox.com/styles/v1/mikelmaron/cjtut3v5u0c281fpoil3w0ch7/tiles/256/{z}/{x}/{y}?fresh=true\&access_token=pk.eyJ1IjoibWlrZWxtYXJvbiIsImEiOiJjaWZlY25lZGQ2cTJjc2trbmdiZDdjYjllIn0.Wx1n0X7aeCQyDTnK6_mrGw --output=kabernet.png
 
 # eldama
 
-#perl stitch.pl --lat=2848 --lon=3977089 --zoom=16 --width=3500 --height=3500 --tileBase=https://api.mapbox.com/styles/v1/mikelmaron/cjl5w24tt08852rlqybihghm2/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWlrZWxtYXJvbiIsImEiOiJjaWZlY25lZGQ2cTJjc2trbmdiZDdjYjllIn0.Wx1n0X7aeCQyDTnK6_mrGw --output=eldama.png
+perl stitch.pl --lat=2848 --lon=3977089 --zoom=16 --width=3500 --height=3500 --tileBase=https://api.mapbox.com/styles/v1/mikelmaron/cjtw9tnnk1p7d1fmwliouj3hk/tiles/256/{z}/{x}/{y}?fresh=true\&access_token=pk.eyJ1IjoibWlrZWxtYXJvbiIsImEiOiJjaWZlY25lZGQ2cTJjc2trbmdiZDdjYjllIn0.Wx1n0X7aeCQyDTnK6_mrGw --output=eldama.png
 
 # mbooni
 
@@ -13,4 +13,10 @@
 
 # wote
 
-perl stitch.pl --lat=-201544 --lon=4185225 --zoom=15 --width=3500 --height=3500 --tileBase=https://api.mapbox.com/styles/v1/mikelmaron/cjla8fi2048wg2sk0c9dfy5y5/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWlrZWxtYXJvbiIsImEiOiJjaWZlY25lZGQ2cTJjc2trbmdiZDdjYjllIn0.Wx1n0X7aeCQyDTnK6_mrGw --output=wote.png
+#perl stitch.pl --lat=-201544 --lon=4185225 --zoom=15 --width=3500 --height=3500 --tileBase=https://api.mapbox.com/styles/v1/mikelmaron/cjrlel3of55yg2spmc1ryf0uc/tiles/256/{z}/{x}/{y}?fresh=true\&access_token=pk.eyJ1IjoibWlrZWxtYXJvbiIsImEiOiJjaWZlY25lZGQ2cTJjc2trbmdiZDdjYjllIn0.Wx1n0X7aeCQyDTnK6_mrGw --output=wote.png
+#perl stitch.pl --lat=-201544 --lon=4185225 --zoom=15 --width=7000 --height=7000 --tileBase=https://api.mapbox.com/styles/v1/mikelmaron/cjrlel3of55yg2spmc1ryf0uc/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWlrZWxtYXJvbiIsImEiOiJjaWZlY25lZGQ2cTJjc2trbmdiZDdjYjllIn0.Wx1n0X7aeCQyDTnK6_mrGw --output=wote.png
+
+
+# wote-center
+
+#perl stitch.pl --lat=-198200 --lon=4188800 --zoom=17 --width=1100 --height=1100 --tileBase=https://api.mapbox.com/styles/v1/mikelmaron/cjrlel3of55yg2spmc1ryf0uc/tiles/256/{z}/{x}/{y}?fresh=true\&access_token=pk.eyJ1IjoibWlrZWxtYXJvbiIsImEiOiJjaWZlY25lZGQ2cTJjc2trbmdiZDdjYjllIn0.Wx1n0X7aeCQyDTnK6_mrGw --output=wote-center.png
